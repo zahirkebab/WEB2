1192486222b66cf7c1b05440cc8e6aeb89a521ee
zahirkebab <317547@via.dk>
2023-02-09 15:27:53 +0100

    exec 1-8

---
+++ 01 Fundamentals/exercise1.js
@@ -0,0 +1,53 @@
+/*
+function range(x){
+    const array=[]
+    return function curried(y,step) {
+     if(step<0) step=step*-1
+    
+        for(x;x<=y;x+=step){
+        array.push(x)
+        }
+        return array
+    }
+   
+        
+}
+
+function sum(array){
+    var sum=0
+    for(const e of array){
+        sum+=e
+    }
+    return sum
+}
+
+
+const rangefrom1=range(1)
+
+console.log(rangefrom1(10,-3))*/
+
+/*
+function reverseArray(array){
+    let array2=[]
+    for(var i=array.length-1;i>=0;i--){
+        array2.push(array[i])
+    }
+    return array2
+}
+
+function reverseArrayInPlace(array){
+    let array2=[]
+    for(var i=array.length-1;i>=0;i--){
+        array2.push(array[i])
+    }
+    array=array2.slice()
+    return array
+}
+
+let arrray=[1,2,3,4,5]
+console.log(reverseArrayInPlace(arrray))
+*/
+
+function arrayToList(array){
+    for (var )
+}
